9b45985717e319bd3e6b03889bc1623d0642e723
FedosimovSV <fedosimovsv@makc.ru>
2015-12-03 13:54:20 +0300

    configure $defaultRoute in module

--- modules/test_task/TestTask.php
+++ modules/test_task/TestTask.php
@@ -5,6 +5,7 @@ namespace app\modules\test_task;
 class TestTask extends \yii\base\Module
 {
     public $controllerNamespace = 'app\modules\test_task\controllers';
+    public $defaultRoute = 'grid';
 
     public function init()
     {
